02342e9be0e7d4be95afe0434323ecdc4f98b437
akshrajput494-star <akshrajput494@gmail.com>
2026-07-27 16:02:22 +0530

    Add files via upload

---
+++ tut5.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int  num1, num2;
+    cout << "enter the value of num 1 \n";
+    cin>>num1;
+    cout << "enter the value of num 2 \n";
+    cin>>num2;
+
+    cout<<"the sum of num1 and num 2 is " <<num1 + num2;
+    /* code */
+    return 0;
+}
